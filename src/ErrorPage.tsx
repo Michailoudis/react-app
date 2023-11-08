174910260128f6b9300e338aa0234e3b649c8237
michailoudit <michailoudit@vmware.com>
2023-11-08 17:42:31 +0200

    add github pages

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -11,7 +11,7 @@ export const ErrorPage: FunctionComponent = (): JSX.Element => {
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{error.message}</i>
       </p>
     </div>
   );
